00b53a0381c4cb4bab79b95cae05a93f99889cd4
RunxinXu <runxinxu@gmail.com>
2023-07-25 09:45:15 +0800

    update

--- fastchat/serve/inference.sh
+++ fastchat/serve/inference.sh
@@ -7,8 +7,14 @@
 #     --temperature 1.0 \
 #     --repetition_penalty 1.0
 
-conda activate llama
+conda activate llama    
 P=/home/xrx/rm/applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/data/data
-d=dahoas_static_hh_bias_vicunaformat_30k_split_2
-r=${d}_vicuna7b_generation.json
-CUDA_VISIBLE_DEVICES=1 python -m fastchat.serve.cli --dataset ${P}/${d} --save ${P}/${r} --responsenum 2 --model-path lmsys/vicuna-7b-v1.3 --temperature 1.0 --repetition_penalty 1.0
+d=dahoas_static_hh_bias_vicunaformat_30k_first15k_split_8
+r=${d}_vicuna_generation_10response.json
+CUDA_VISIBLE_DEVICES=7 python -m fastchat.serve.cli \
+    --dataset ${P}/${d} \
+    --save ${P}/${r} \
+    --responsenum 10 \
+    --model-path lmsys/vicuna-7b-v1.3 \
+    --temperature 1.0 \
+    --repetition_penalty 1.0
